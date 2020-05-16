596d45b1eef2dc74635b568f3ab7b0897c62f978
Ivan Chechkin <i.g.chechkin@yandex.ru>
2020-05-16 10:12:00 +0300

    Added urls and models for passes

--- manager/models.py
+++ manager/models.py
@@ -15,3 +15,18 @@ class News(models.Model):
     publicationTitle = models.CharField(max_length=50)
     publicationText = models.TextField(max_length=5000)
 
+
+class Pass(models.Model):
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE())
+    cr_date = models.DateTimeField(auto_now=True)
+    status = models.TextField(max_length=7)
+    target = models.TextField(max_length=6)
+    name = models.TextField(null=True, blank=True, max_length=20)
+    surname = models.TextField(null=True, blank=True, max_length=20)
+    patronymic = models.TextField(null=True, blank=True, max_length=20)
+    model = models.TextField(null=True, blank=True, max_length=20)
+    color = models.TextField(null=True, blank=True, max_length=20)
+    number = models.TextField(null=True, blank=True, max_length=20)
+    aim = models.TextField(max_length=40)
+
+
