e7f6c15d9b1d1c9508072e3863ba9ff0a93a1c8c
Andrew Smirnov <smirnov@informatics.ru>
2020-06-06 07:44:13 +0300

    Add docs generation to CD

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 cd /s104_juk/ || exit 1
+cd docs && make html
+cp -r _build/html ../staticroot/
+cd ..
 python manage.py migrate
 python manage.py collectstatic --noinput
 daphne -b 0.0.0.0 -p 8087 --access-log /var/log/s104_juk/daphne_access.log juk.asgi:application
